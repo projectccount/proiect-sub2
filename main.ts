ca0bb9260c19537d7536f80da5583018f0afa77f
projectccount <81797659+projectccount@users.noreply.github.com>
2021-04-02 09:46:33 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,170 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber > 30) {
-        music.playTone(262, music.beat(BeatFraction.Double))
+    if (input.buttonIsPressed(Button.B)) {
+        basic.clearScreen()
+        if (receivedNumber == 1) {
+            basic.showLeds(`
+                . . # . .
+                . # # . .
+                . . # . .
+                . . # . .
+                . # # # .
+                `)
+        } else if (receivedNumber == 2) {
+            basic.showLeds(`
+                . # # . .
+                . . . # .
+                . . # . .
+                . # . . .
+                . # # # .
+                `)
+        } else if (receivedNumber == 3) {
+            basic.showLeds(`
+                . # # # .
+                . . . # .
+                . . # . .
+                . . . # .
+                . # # # .
+                `)
+        } else if (receivedNumber == 4) {
+            basic.showLeds(`
+                . # . # .
+                . # . # .
+                . # # # .
+                . . . # .
+                . . . # .
+                `)
+        } else if (receivedNumber == 5) {
+            basic.showLeds(`
+                . # # # .
+                . # . . .
+                . # # # .
+                . . . # .
+                . # # # .
+                `)
+        } else if (receivedNumber == 6) {
+            basic.showLeds(`
+                . # # # .
+                . # . . .
+                . # # # .
+                . # . # .
+                . # # # .
+                `)
+        } else if (receivedNumber == 7) {
+            basic.showLeds(`
+                . # # # .
+                . . . # .
+                . . . # .
+                . . . # .
+                . . . # .
+                `)
+        } else if (receivedNumber == 8) {
+            basic.showLeds(`
+                . # # # .
+                . # . # .
+                . # # # .
+                . # . # .
+                . # # # .
+                `)
+        } else if (receivedNumber == 9) {
+            basic.showLeds(`
+                . # # # .
+                . # . # .
+                . # # # .
+                . . . # .
+                . . . # .
+                `)
+        } else if (receivedNumber == 10) {
+            basic.showLeds(`
+                . # # # #
+                # # # . #
+                . # # . #
+                . # # . #
+                # # # # #
+                `)
+        } else if (receivedNumber == 11) {
+            basic.showLeds(`
+                . # . . #
+                # # . # #
+                . # . . #
+                . # . . #
+                # # . # #
+                `)
+        } else if (receivedNumber == 12) {
+            basic.showLeds(`
+                # . # # #
+                # . . . #
+                # . # # #
+                # . # . .
+                # . # # #
+                `)
+        } else if (receivedNumber == 13) {
+            basic.showLeds(`
+                # . # # #
+                # . . . #
+                # . # # #
+                # . . . #
+                # . # # #
+                `)
+        } else if (receivedNumber == 14) {
+            basic.showLeds(`
+                # . # . #
+                # . # . #
+                # . # # #
+                # . . . #
+                # . . . #
+                `)
+        } else if (receivedNumber == 15) {
+            basic.showLeds(`
+                # . # # #
+                # . # . .
+                # . # # #
+                # . . . #
+                # . # # #
+                `)
+        } else if (receivedNumber == 16) {
+            basic.showLeds(`
+                # . # # #
+                # . # . .
+                # . # # #
+                # . # . #
+                # . # # #
+                `)
+        } else if (receivedNumber == 17) {
+            basic.showLeds(`
+                # . # # #
+                # . . . #
+                # . . # .
+                # . . # .
+                # . . # .
+                `)
+        } else if (receivedNumber == 18) {
+            basic.showLeds(`
+                # . # # #
+                # . # . #
+                # . # # #
+                # . # . #
+                # . # # #
+                `)
+        } else if (receivedNumber == 19) {
+            basic.showLeds(`
+                # . # # #
+                # . # . #
+                # . # # #
+                # . . . #
+                # . . . #
+                `)
+        } else if (receivedNumber == 20) {
+            basic.showLeds(`
+                # # # # #
+                . # # . #
+                # # # . #
+                # . # . #
+                # # # # #
+                `)
+        }
     }
 })
-input.onButtonPressed(Button.A, function () {
-    radio.setGroup(125)
+basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 20) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 102)
         basic.pause(1000)
@@ -17,172 +177,6 @@ input.onButtonPressed(Button.A, function () {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 152)
         basic.showIcon(IconNames.No)
     }
-    radio.sendNumber(input.temperature())
-    radio.sendValue("name", maqueen.Ultrasonic(PingUnit.Centimeters))
-})
-radio.onReceivedValue(function (name, value) {
-    if (value == 1) {
-        basic.showLeds(`
-            . . # . .
-            . # # . .
-            . . # . .
-            . . # . .
-            . # # # .
-            `)
-    } else if (value == 2) {
-        basic.showLeds(`
-            . # # . .
-            . . . # .
-            . . # . .
-            . # . . .
-            . # # # .
-            `)
-    } else if (value == 3) {
-        basic.showLeds(`
-            . # # # .
-            . . . # .
-            . . # . .
-            . . . # .
-            . # # # .
-            `)
-    } else if (value == 4) {
-        basic.showLeds(`
-            . # . # .
-            . # . # .
-            . # # # .
-            . . . # .
-            . . . # .
-            `)
-    } else if (value == 5) {
-        basic.showLeds(`
-            . # # # .
-            . # . . .
-            . # # # .
-            . . . # .
-            . # # # .
-            `)
-    } else if (value == 6) {
-        basic.showLeds(`
-            . # # # .
-            . # . . .
-            . # # # .
-            . # . # .
-            . # # # .
-            `)
-    } else if (value == 7) {
-        basic.showLeds(`
-            . # # # .
-            . . . # .
-            . . . # .
-            . . . # .
-            . . . # .
-            `)
-    } else if (value == 8) {
-        basic.showLeds(`
-            . # # # .
-            . # . # .
-            . # # # .
-            . # . # .
-            . # # # .
-            `)
-    } else if (value == 9) {
-        basic.showLeds(`
-            . # # # .
-            . # . # .
-            . # # # .
-            . . . # .
-            . . . # .
-            `)
-    } else if (value == 10) {
-        basic.showLeds(`
-            . # # # #
-            # # # . #
-            . # # . #
-            . # # . #
-            # # # # #
-            `)
-    } else if (value == 11) {
-        basic.showLeds(`
-            . # . . #
-            # # . # #
-            . # . . #
-            . # . . #
-            # # . # #
-            `)
-    } else if (value == 12) {
-        basic.showLeds(`
-            # . # # #
-            # . . . #
-            # . # # #
-            # . # . .
-            # . # # #
-            `)
-    } else if (value == 13) {
-        basic.showLeds(`
-            # . # # #
-            # . . . #
-            # . # # #
-            # . . . #
-            # . # # #
-            `)
-    } else if (value == 14) {
-        basic.showLeds(`
-            # . # . #
-            # . # . #
-            # . # # #
-            # . . . #
-            # . . . #
-            `)
-    } else if (value == 15) {
-        basic.showLeds(`
-            # . # # #
-            # . # . .
-            # . # # #
-            # . . . #
-            # . # # #
-            `)
-    } else if (value == 16) {
-        basic.showLeds(`
-            # . # # #
-            # . # . .
-            # . # # #
-            # . # . #
-            # . # # #
-            `)
-    } else if (value == 17) {
-        basic.showLeds(`
-            # . # # #
-            # . . . #
-            # . . # .
-            # . . # .
-            # . . # .
-            `)
-    } else if (value == 18) {
-        basic.showLeds(`
-            # . # # #
-            # . # . #
-            # . # # #
-            # . # . #
-            # . # # #
-            `)
-    } else if (value == 19) {
-        basic.showLeds(`
-            # . # # #
-            # . # . #
-            # . # # #
-            # . . . #
-            # . . . #
-            `)
-    } else if (value == 20) {
-        basic.showLeds(`
-            # # # # #
-            . # # . #
-            # # # . #
-            # . # . #
-            # # # # #
-            `)
-    }
-})
-basic.forever(function () {
-	
+    radio.setGroup(125)
+    radio.sendNumber(maqueen.Ultrasonic(PingUnit.Centimeters))
 })
